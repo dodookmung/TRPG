80cd199494a6962d1198d265acb7db1c2c1803d6
dodookmung <dodookmungx@gmail.com>
2025-03-25 09:04:13 +0900

    Class와 Type Annotations

--- main.py
+++ main.py
@@ -24,13 +24,38 @@ def ChooseClass():
     choice = ClassType.NoneType
     
     if user_input == '1':
-            choice = ClassType.Knight
+        choice = ClassType.Knight
     elif user_input == '2':
-            choice = ClassType.Archer
+        choice = ClassType.Archer
     elif user_input == '3':
-            choice = ClassType.Mage
+        choice = ClassType.Mage
     
     return choice
+
+
+class Player:
+    def __init__(self):
+        self.hp = 0
+        self.attack = 0
+
+
+def CreatePlayer(choice:ClassType, player:Player):
+    hp = 0
+    attack = 0
+    # 기사(100/10) 궁수(75/12) 법사(50/15)
+    if choice == ClassType.Knight:
+        player.hp=100
+        player.attack=10
+    elif choice == ClassType.Archer:
+        player.hp=75
+        player.attack=12
+    elif choice == ClassType.Mage:
+        player.hp=50
+        player.attack=15
+    else:
+        player.hp=0
+        player.attack=0
+          
 
 def main():
     
@@ -42,7 +67,13 @@ def main():
             ClearScreen()
             print('[ 잘못된 입력입니다. 다시 선택해주세요.]')
         elif choice != ClassType.NoneType:
-              break
+            # 캐릭터 생성
+            player = Player()
+            CreatePlayer(choice, player)
+            
+            print(f'HP:{player.hp}, Attack:{player.attack}')
+
+            # 필드로 가서 몬스터랑 싸운다
         
         
 
